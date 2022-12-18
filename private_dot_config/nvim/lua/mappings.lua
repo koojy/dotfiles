89fbbe4f84b11c74e176a4da952641171caf6b9a
Koji Murakami <koji.murakami3@gmail.com>
2022-12-18 18:17:53 +0900

    nvimのlua配下を追加

---
+++ private_dot_config/nvim/lua/mappings.lua
@@ -0,0 +1,28 @@
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = ' '
+
+map('n', '<Space>', '', {})
+
+map('n', '<C-H>', '<C-W><C-H>', opts)
+map('n', '<C-J>', '<C-W><C-J>', opts)
+map('n', '<C-K>', '<C-W><C-K>', opts)
+map('n', '<C-L>', '<C-W><C-L>', opts)
+
+map('i', 'jj', '<Esc>', opts)
+
+map('n', ';', ':', opts)
+map('n', ':', ';', opts)
+
+map('n', '<Leader>w', ':w<CR>', opts)
+map('n', '<Leader>q', ':q<CR>', opts)
+map('n', 'L', 'gt', opts)
+map('n', 'H', 'gT', opts)
+
+map('v', 'y', '"+y', opts)
+map('v', 'p', '"+p', opts)
+
+map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+map("n", "<C-o>", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
---
+++ private_dot_config/nvim/lua/options.lua
@@ -0,0 +1,14 @@
+local o = vim.opt
+
+o.number = true
+o.expandtab = true
+o.tabstop = 2
+o.softtabstop = 0
+o.shiftwidth = 2
+o.cursorline = true
+o.ignorecase = true
+o.encoding = 'utf-8'
+o.termguicolors = true
+o.list = true
+o.listchars:append("space:⋅")
+o.listchars:append("eol:↴")
